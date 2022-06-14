d42f173377fa938a79075ba830a171959b78cd95
Ali Jabbary <light.knight32@live.com>
2022-06-14 10:58:58 +0430

    Virastar page created

--- virastar/views.py
+++ virastar/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the virastar index.")
+    return render(request, 'virastar.html')
